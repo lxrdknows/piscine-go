fd89f083bf3d7cac5d11429070cfb43d08c06530
lxrdknows <3ppashok@gmail.com>
2019-10-16 21:29:52 +0600

    2

--- printcomb.go
+++ printcomb.go
@@ -1,12 +1,12 @@
 package piscine
 
-import "fgithub.com/01-edu/z01"
+import "github.com/01-edu/z01"
 
 func PrintComb() {
 	next := false
 	for a = '0'; a <= '9'; a++ {
-		for b := a + 1; b <= '9'; b++ {
-			for c := b + 1; c <= '9'; c++ {
+		for b := a+1; b <= '9'; b++ {
+			for c := b+1; c <= '9'; c++ {
 				if next {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
@@ -15,8 +15,9 @@ func PrintComb() {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
+
+				}
 			}
 		}
+		z01.PrintRune(10)
 	}
-	z01.PrintRune(10)
-}
