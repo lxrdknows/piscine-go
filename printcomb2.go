5917aedbf30c2cdc6eb249e3dbcd4b6d9743c334
lxrdknows <3ppashok@gmail.com>
2019-10-16 23:39:33 +0600

    2

--- printcomb2.go
+++ printcomb2.go
@@ -5,6 +5,7 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for a := '0'; a <= '9'; a = a + 1 {
 		for b := '0'; b <= '9'; c = c + 1 {
+			d := b + 1
 			for ; d <= '9'; d = d + 1 {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
@@ -23,6 +24,5 @@ func PrintComb2() {
 	
 }
 z01.PrintRune(10)
-
 
 }
