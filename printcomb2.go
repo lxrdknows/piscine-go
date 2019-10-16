458b7303e0af3f225a53b04002ef4bdf550ad205
lxrdknows <3ppashok@gmail.com>
2019-10-16 23:43:34 +0600

    2

--- printcomb2.go
+++ printcomb2.go
@@ -3,9 +3,11 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
+
 	for a := '0'; a <= '9'; a = a + 1 {
-		for b := '0'; b <= '9'; c = c + 1 {
+		for b := '0'; b <= '9'; b = b + 1 {
 			d := b + 1
+			for c := a; c <= '9'; c = c +1 {
 			for ; d <= '9'; d = d + 1 {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
