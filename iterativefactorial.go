4c5b6119b24585d5cbf424603f9c5789a2351e61
lxrdknows <3ppashok@gmail.com>
2019-10-21 17:05:01 +0600

    2211

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,18 +1,14 @@
-package main
-
-import "fmt"
+package piscine
 
 func IterativeFactorial(nb int) int {
 	if nb < 0 || nb > 15 {
 		return 0
 	}
-	if nb == 1 || nb == 0 {
-		return 1
-	}
-	return nb * RecursiveFactorial(nb-1)
-}
+	a := 1
+	b := nb
+	for nb := 1; nb <= b; nb++ {
 
-func main() {
-	arg := 4
-	fmt.Println(piscine.IterativeFactorial(arg))
+		a = a * nb
+	}
+	return a
 }
