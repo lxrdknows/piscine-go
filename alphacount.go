53520dacf6c9c4c46bd4c6e344468b2a6c139071
lxrdknows <3ppashok@gmail.com>
2019-10-22 15:49:32 +0600

    22

---
+++ alphacount.go
@@ -0,0 +1,21 @@
+package piscine
+
+
+func AlphaCount(str string) int {
+	counter := 0
+	for _, count := range str {
+		if count >= 'A' && count <= 'Z' {
+
+			counter++
+		} else if count >= 'a' && count <= 'z' {
+			counter++
+
+		}
+		
+		}
+
+	}
+	return counter
+
+}
+
