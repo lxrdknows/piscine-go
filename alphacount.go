f1af9209438bc2311de21639570d17181a30cb7f
lxrdknows <3ppashok@gmail.com>
2019-10-22 15:51:40 +0600

    22

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 
 func AlphaCount(str string) int {
 	counter := 0
-	return counter
+	
 	for _, count := range str {
 		if count >= 'A' && count <= 'Z' {
 
@@ -14,7 +14,7 @@ func AlphaCount(str string) int {
 		}
 		
 		}
-
+		return counter
 	}
 	
 
